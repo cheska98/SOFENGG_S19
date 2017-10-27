f9891368e4ad205ee939a5c8e6b30f0bd958a121
kateeriveDLSU <30729847+kateeriveDLSU@users.noreply.github.com>
2017-10-27 16:09:57 +0800

    DeletedJavaFiles
    deleted java files that was converted to fxml

--- src/view/TestDriver.java
+++
@@ -1,9 +0,0 @@
-import javafx.application.Application;
-public class TestDriver {
-
-	public static void main(String[] args) {
-          //Application.launch(DebtListPanel.class,args);
-         //Application.launch(DisplayPanel.class,args);
-		Application.launch(InventoryPanel.class,args);
-     }
-}
