08612038dea6a78e28c071c023b4e0c4f81205ad
franchong <franchesca_chong@yahoo.com>
2017-11-06 23:56:37 +0800

    Added model folder
    Added Reports
    Moved SaleEntry here

---
+++ src/model/Report.java
@@ -0,0 +1,31 @@
+package model;
+
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Report {
+	
+	public final SimpleStringProperty item = new SimpleStringProperty("");;
+	public final SimpleIntegerProperty quantity = new SimpleIntegerProperty();;
+	public final SimpleFloatProperty unitCost = new SimpleFloatProperty();;
+	public final SimpleFloatProperty price = new SimpleFloatProperty();;
+	
+	
+	public String getItem() {
+		return item.get();
+	}
+	
+	public Integer getQuantity() {
+		return quantity.get();
+	}
+	
+	public Float getUnitCost() {
+		return unitCost.get();
+	}
+	
+	public Float getPrice() {
+		return price.get();
+	}
+	
+}
