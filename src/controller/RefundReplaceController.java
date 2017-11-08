887d63f31ec9b8b6f8b32b6199ba20521d56d83b
franchong <franchesca_chong@yahoo.com>
2017-11-08 08:28:02 +0800

    Edited RefundReplace
    Added toggle group for radio buttons

--- src/controller/RefundReplaceController.java
+++ src/controller/RefundReplaceController.java
@@ -10,39 +10,35 @@ import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 
 public class RefundReplaceController implements Initializable {
 
-    @FXML
-    private AnchorPane refundReplace;
+    @FXML private AnchorPane refundReplace;
+    @FXML private TableView<?> tableRR;
+    @FXML private TableColumn<?, ?> itemColumn;
+    @FXML private TableColumn<?, ?> quantityColumn;
+    @FXML private RadioButton refundRB;
+    @FXML private RadioButton replaceRB;
+    @FXML private Button okButton;
+    private ToggleGroup refundOrReplace;
 
-    @FXML
-    private RadioButton replaceRB;
-
-    @FXML
-    private RadioButton refundRB;
-
-    @FXML
-    private Button okButton;
-
-    @FXML
-    private TableView<?> tableRR;
-
-    @FXML
-    private TableColumn<?, ?> itemColumn;
-
-    @FXML
-    private TableColumn<?, ?> quantityColumn;
-
+    
     @FXML
     void handleOK(ActionEvent event) {
+    	
+    	refundOrReplace.selectToggle(refundRB);
 
     }
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+    	refundOrReplace = new ToggleGroup();
+    	refundRB.setToggleGroup(refundOrReplace);
+    	replaceRB.setToggleGroup(refundOrReplace);
+    	refundOrReplace.selectToggle(refundRB);
 		
 	}
 
