1f8dd51fa32e385be578d9ed4119f30d48c004a1
celina-iris <celina_brito@dlsu.edu.ph>
2017-11-20 21:15:50 +0800

    Add files via upload

---
+++ src/model/TransactionItem.java
@@ -0,0 +1,50 @@
+package model;
+import java.util.ArrayList;
+
+public class TransactionItem {
+	int transactionID;
+	int itemID;
+	float unitPrice;
+	int quantity;
+	String action;
+	
+	public int getTransactionID() {
+		return transactionID;
+	}
+
+	public void setTransactionID(int transactionID) {
+		this.transactionID = transactionID;
+	}
+
+	public int getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(int itemID) {
+		this.itemID = itemID;
+	}
+
+	public float getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(float unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+}
