82d3b55ca26e1803604681645c422f122b660282
franchong <franchesca_chong@yahoo.com>
2017-12-14 02:02:11 +0800

    Edited Views and SuperMainController
    Added button change and help button in Main.fxml
    Added scrollpane for the tables

--- src/controller/SuperMainController.java
+++ src/controller/SuperMainController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
@@ -19,14 +20,16 @@ public class SuperMainController {
     @FXML private Label label1;
     @FXML private Label nameLabel;
     @FXML private Label currDateLabel;
-    @FXML private Button salesButton;
+    @FXML private Button helpButton;
+    @FXML private Button transactionButton; ////////// edit from 'sales' to 'transaction'
     @FXML private Button displayButton;
     @FXML private Button logoutButton;
     @FXML private Button inventoryButton;
     @FXML private Button debtListButton;
     @FXML private Button reportsButton;
     @FXML private Button refundReplaceButton;
-    @FXML private ImageView salesIcon;
+    @FXML private ImageView helpIcon; //////////// add this
+    @FXML private ImageView transactionIcon; ////////// edit from 'sales' to 'transaction'
     @FXML private ImageView displayIcon;
     @FXML private ImageView inventoryIcon;
     @FXML private ImageView reportsIcon;
@@ -48,18 +51,35 @@ public class SuperMainController {
 	@FXML DebtListController debtListController;
 	@FXML RefundReplaceController refundReplaceController;
 	InitPanelController initialController;
-	
+/////////////// from this
+    private Image transactionIconLight;
+    private Image displayIconLight;
+    private Image inventoryIconLight;
+    private Image reportsIconLight;
+    private Image debtListIconLight;
+    private Image refundReplaceIconLight;
+    private Image transactionIconDark;
+    private Image displayIconDark;
+    private Image inventoryIconDark;
+    private Image reportsIconDark;
+    private Image debtListIconDark;
+    private Image refundReplaceIconDark;
+///////////////// to this	
 	private Calendar now = Calendar.getInstance();
 	private String currDate = null;
 	private String month = null;
 	private String day = null;
 	private String year = Integer.toString(now.get(Calendar.YEAR));
+	private int pane = 0; ///////////// add this
+	private Alert alert; ///////////// add this
 	
 	
 	@FXML
 	private void initialize() {
 		
 		setCurrDate();
+		setLightImages(); /////////// add this
+		setDarkImages(); /////////// add this
 		setTransactionVisible();
 		
 	}
@@ -104,7 +124,30 @@ public class SuperMainController {
 		currDateLabel.setText(currDate);
 		
 	}
+	/////////// from this
+	private void setLightImages() {
+		
+		transactionIconLight = new Image(getClass().getResource("../img/transaction.png").toExternalForm());
+		displayIconLight = new Image(getClass().getResource("../img/display.png").toExternalForm());
+		inventoryIconLight = new Image(getClass().getResource("../img/inventory.png").toExternalForm());
+		reportsIconLight = new Image(getClass().getResource("../img/reports.png").toExternalForm());
+		debtListIconLight = new Image(getClass().getResource("../img/debt_list.png").toExternalForm());
+		refundReplaceIconLight = new Image(getClass().getResource("../img/refund-replace.png").toExternalForm());
+		
+	}
 	
+	
+	private void setDarkImages() {
+		
+		transactionIconDark = new Image(getClass().getResource("../img/transaction_selected.png").toExternalForm());
+		displayIconDark = new Image(getClass().getResource("../img/display_selected.png").toExternalForm());
+		inventoryIconDark = new Image(getClass().getResource("../img/inventory_selected.png").toExternalForm());
+		reportsIconDark = new Image(getClass().getResource("../img/reports_selected.png").toExternalForm());
+		debtListIconDark = new Image(getClass().getResource("../img/debt_list_selected.png").toExternalForm());
+		refundReplaceIconDark = new Image(getClass().getResource("../img/refund-replace_selected.png").toExternalForm());
+		
+	}
+	////////////// to this
 	@FXML
     void logoutAction(ActionEvent event) {
 	
@@ -118,7 +161,72 @@ public class SuperMainController {
 		}
 		
     }
-    
+	/////////////// from this
+	@FXML
+	void helpAction(ActionEvent event) {
+		
+		switch(pane) {
+			case 1: alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("HELP");
+					alert.setHeaderText("Transaction: where the transaction of each customer can be made");
+					alert.setContentText("Add an item to the cart by entering the name of the item, then click the 'Add to Cart' button.\n\n"
+										+ "Change the quantity by clicking the item's quantity and entering the quantity you want.\n\n"
+										+ "Discounts on each item can be applied by clicking the unit cost and entering the doscounted unit cost\n\n"
+										+ "Delete an item in the cart by checking the checkbox and clicking the 'Delete' button.\n\n"
+										+ "After adding all the items in the cart, click the 'Complete' button.\n\n"
+										+ "When completing a transaction, you can enter the amount paid by the customer or choose to add the customer to the debt list.");
+					alert.showAndWait();
+				break;
+			case 2: alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("HELP");
+					alert.setHeaderText("Display: where all the items in the store can be viewed\n\n");
+					alert.setContentText("Add an item in the display by entering the name of the item, price, and quantity and clicking the 'Add Item' button.\n\n"
+										+ "Search an item by entering a keyword and clicking the 'Search' button.\n\n"
+										+ "Clear the search table by clicking the 'CLEAR' button.\n\n"
+										+ "Save all the changes done by clicking the 'Save Changes' button.\n\n"
+										+ "Delete an item in the display by checking the checkbox and clicking the 'Delete' button.");
+					alert.showAndWait();
+				break;
+			case 3: alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("HELP");
+					alert.setHeaderText("Inventory: where all the items in the inventory can be viewed");
+					alert.setContentText("Add an item in the inventory by entering the name of the item, price, and quantity and clicking the 'Add Item' button.\n\n"
+										+ "Search an item by entering a keyword and clicking the 'Search' button.\n\n"
+										+ "Clear the search table by clicking the 'CLEAR' button.\n\n"
+										+ "Save all the changes done by clicking the 'Save Changes' button.\n\n"
+										+ "Delete an item in the display by checking the checkbox and clicking the 'Delete' button.");
+					alert.showAndWait();
+				break;
+			case 4: alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("HELP");
+					alert.setHeaderText("Reports: where the reports can be viewed or downloaded");
+					alert.setContentText("View a daily report by select a date, then click the 'Ok' button.\n\n"
+										+ "View a monthly report by select a month and year, then click the 'Ok' button.\n\n"
+										+ "View top items for the day by clicking the 'View Top Items' button.\n\n"
+										+ "Download a report by clicking the 'Download' button, the excel file can be found in __.");
+					alert.showAndWait();
+				break;
+			case 5: alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("HELP");
+					alert.setHeaderText("Debt List: where the debts of each customer can be viewed\n\n");
+					alert.setContentText("Search a transaction ID or customer name by entering either one and clicking the 'Search' button.\n\n"
+										+ "Double click a row to view all pending transactions of a customer.\n\n"
+										+ "To mark a customer's debt as paid, click the 'Paid' button.");
+					alert.showAndWait();
+				break;
+			default: alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("HELP");
+					alert.setHeaderText("Refund/Replace: where items can be replaced or refunded\n\n");
+					alert.setContentText("Search a transaction ID by entering it and clicking the 'Search' button to view list of items bought.\n\n"
+										+ "Select an item to be refunded or replaced by selecting a row.\n\n"
+										+ "Choose between refund and replace by selecting a button.\n\n"
+										+ "Enter the quantity to be refunded or replaced by entering it and clicking the button.");
+					alert.showAndWait();
+				break;
+		}
+		
+	}
+    ////////// to this
     @FXML
     void transactionAction(ActionEvent event) {
     	setTransactionVisible();
@@ -148,35 +256,50 @@ public class SuperMainController {
     void refundReplaceAction(ActionEvent event) {
     	setRefundReplaceVisible();
     }
-	
-	public void setTransactionVisible() {
+	///////////////////// from this 
+	private void setTransactionVisible() {
+		
+    	pane = 1;
+    	setTransactionSelected();
 		transaction.setVisible(true);
 		display.setVisible(false);
 		inventory.setVisible(false);
 		reports.setVisible(false);
 		debtList.setVisible(false);
 		refundReplace.setVisible(false);
+		
 	}
 	
-	public void setDisplayVisible() {
+	private void setDisplayVisible() {
+		
+    	pane = 2;
+    	setDisplaySelected();
 		transaction.setVisible(false);
 		display.setVisible(true);
 		inventory.setVisible(false);
 		reports.setVisible(false);
 		debtList.setVisible(false);
 		refundReplace.setVisible(false);
+		
 	}
 
-	public void setInventoryVisible() {
+	private void setInventoryVisible() {
+		
+    	pane = 3;
+    	setInventorySelected();
 		transaction.setVisible(false);
 		display.setVisible(false);
 		inventory.setVisible(true);
 		reports.setVisible(false);
 		debtList.setVisible(false);
 		refundReplace.setVisible(false);
+		
 	}
 
-	public void setReportsVisible() {
+	private void setReportsVisible() {
+		
+    	pane = 4;
+    	setReportsSelected();
 		reportsController.initialize();
 		transaction.setVisible(false);
 		display.setVisible(false);
@@ -184,24 +307,99 @@ public class SuperMainController {
 		reports.setVisible(true);
 		debtList.setVisible(false);
 		refundReplace.setVisible(false);
+		
 	}
 
-	public void setDebtListVisible() {
+	private void setDebtListVisible() {
+		
+    	pane = 5;
+    	setDebtListSelected();
 		transaction.setVisible(false);
 		display.setVisible(false);
 		inventory.setVisible(false);
 		reports.setVisible(false);
 		debtList.setVisible(true);
 		refundReplace.setVisible(false);
+		
 	}
 
-	public void setRefundReplaceVisible() {
+	private void setRefundReplaceVisible() {
+		
+    	pane = 6;
+    	setRefindReplaceSelected();
 		transaction.setVisible(false);
 		display.setVisible(false);
 		inventory.setVisible(false);
 		reports.setVisible(false);
 		debtList.setVisible(false);
 		refundReplace.setVisible(true);
+		
 	}
 	
+	private void setTransactionSelected() {
+		
+    	transactionIcon.setImage(transactionIconDark);
+    	displayIcon.setImage(displayIconLight);
+    	inventoryIcon.setImage(inventoryIconLight);
+    	reportsIcon.setImage(reportsIconLight);
+    	debtListIcon.setImage(debtListIconLight);
+    	refundReplaceIcon.setImage(refundReplaceIconLight);
+    	
+	}
+	
+	private void setDisplaySelected() {
+
+    	transactionIcon.setImage(transactionIconLight);
+    	displayIcon.setImage(displayIconDark);
+    	inventoryIcon.setImage(inventoryIconLight);
+    	reportsIcon.setImage(reportsIconLight);
+    	debtListIcon.setImage(debtListIconLight);
+    	refundReplaceIcon.setImage(refundReplaceIconLight);
+    	
+	}
+	
+	private void setInventorySelected() {
+
+    	transactionIcon.setImage(transactionIconLight);
+    	displayIcon.setImage(displayIconLight);
+    	inventoryIcon.setImage(inventoryIconDark);
+    	reportsIcon.setImage(reportsIconLight);
+    	debtListIcon.setImage(debtListIconLight);
+    	refundReplaceIcon.setImage(refundReplaceIconLight);
+    	
+	}
+	
+	private void setReportsSelected() {
+
+    	transactionIcon.setImage(transactionIconLight);
+    	displayIcon.setImage(displayIconLight);
+    	inventoryIcon.setImage(inventoryIconLight);
+    	reportsIcon.setImage(reportsIconDark);
+    	debtListIcon.setImage(debtListIconLight);
+    	refundReplaceIcon.setImage(refundReplaceIconLight);
+    	
+	}
+	
+	private void setDebtListSelected() {
+
+    	transactionIcon.setImage(transactionIconLight);
+    	displayIcon.setImage(displayIconLight);
+    	inventoryIcon.setImage(inventoryIconLight);
+    	reportsIcon.setImage(reportsIconLight);
+    	debtListIcon.setImage(debtListIconDark);
+    	refundReplaceIcon.setImage(refundReplaceIconLight);
+    	
+	}
+	
+	private void setRefindReplaceSelected() {
+		
+    	transactionIcon.setImage(transactionIconLight);
+    	displayIcon.setImage(displayIconLight);
+    	inventoryIcon.setImage(inventoryIconLight);
+    	reportsIcon.setImage(reportsIconLight);
+    	debtListIcon.setImage(debtListIconLight);
+    	refundReplaceIcon.setImage(refundReplaceIconDark);
+    	
+	}
+	///////////// to this
 }
