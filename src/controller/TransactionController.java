297d580e49f9ba12d96e1cfcc43a31e1b019400e
isakyaki <pamela_ortiz@dlsu.edu.ph>
2017-12-13 17:56:27 +0800

    push push

--- src/controller/TransactionController.java
+++ src/controller/TransactionController.java
@@ -1,8 +1,9 @@
 package controller;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.text.DecimalFormat;
-import java.util.Optional;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.beans.property.SimpleFloatProperty;
@@ -30,89 +31,73 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.converter.FloatStringConverter;
 import javafx.util.converter.IntegerStringConverter;
-import model.SaleEntry;
+import model.Model;
+import model.entity.Item;
+import model.entity.TransactionItem;
+import model.entries.SaleEntry;
+import model.service.ItemService;
+import org.controlsfx.control.textfield.TextFields;
 
 public class TransactionController implements Initializable {
-
+	
     @FXML
     private AnchorPane sales;
-
     @FXML
     private TableView<SaleEntry> salesTable;
-
     @FXML
     private TableColumn<SaleEntry, String> itemCol;
-
     @FXML
     private TableColumn<SaleEntry, Integer> quantityCol;
-
     @FXML
     private TableColumn<SaleEntry, Float> costCol;
-
     @FXML
     private TableColumn<SaleEntry, Float> priceCol;
-
     @FXML
     private TableColumn<SaleEntry, Boolean> checkColumn;
-
     @FXML
     private TextField itemText;
-    
     @FXML
     private Button btnCart;
-    
     @FXML
     private Button btnComplete;
-
     @FXML
     private TextField totalAmount;
-    
     @FXML
     private TextField paidTF;
-
     @FXML
     private Button btnOK;
-
     @FXML
     private ComboBox<String> debtListCB;
-
     @FXML
     private Button btnCancel;
-    
     @FXML
     private Button btnDelete;
-
     @FXML
     private AnchorPane completePopup;
-    
     ObservableList<String> debtCustomer;
-
     ObservableList<SaleEntry> data;
-
-    SaleEntry se;
-    SaleEntry fe;
-    
     private static float ucost = 0;
     private static int qty = 1;
     private int changeqty = 1;
     private float changeucost = 0;
     SaleEntry newEntry;
     DecimalFormat f = new DecimalFormat("##.00");
+    private static Model model; 
     
     public TransactionController() {
-    	
-    	se = new SaleEntry();
-    	fe = new SaleEntry();
-    	
-        se.setItemName(new SimpleStringProperty("Test"));
-        se.setUcost(new SimpleFloatProperty(35));		
+    	System.out.println("2");
+        //se.setItemName(new SimpleStringProperty("Test"));
+        //se.setUcost(new SimpleFloatProperty(35));		
         
 
-        fe.setItemName(new SimpleStringProperty("tt"));
-        fe.setUcost(new SimpleFloatProperty(45));	
+        //fe.setItemName(new SimpleStringProperty("tt"));
+        //fe.setUcost(new SimpleFloatProperty(45));	
     	
     }
-
+    public void setModel(Model model) { 
+    	this.model = model; 
+    }
+    
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
@@ -205,32 +190,37 @@ public class TransactionController implements Initializable {
 	        		cellData -> cellData.getValue().selectedProperty()
 	            );
 	        
-	        data = FXCollections.observableArrayList(); // create the data
-
+	        data = FXCollections.observableArrayList(); // create the data Insert DB here
 		    salesTable.setItems(data); 
-		    
 		    debtCustomer = FXCollections.observableArrayList();
-	    	
 	    	debtCustomer.add("Takashi Shirogane");
 	    	debtCustomer.add("Keith Kogane");
-	    	
-	    	
 	    	debtListCB.setItems(debtCustomer);
-	    	
-
 	    	debtListCB.getItems().add("New Customer in Debt");
-	    	
 	    	completePopup.setVisible(false);
-	    	
 	    	totalAmount.setText("00.00");
+
+	    	TextFields.bindAutoCompletion(itemText, "hello", "poop"); //this is will do the auto-complete search
 	    	
 	}
 
 	@FXML
     void handlecomplete(ActionEvent event) {
     	
+		if(data.isEmpty()) {
+			
+			Alert alert = new Alert(AlertType.INFORMATION);
+	    	alert.setTitle("Cannot Complete Action!");
+	    	alert.setHeaderText(null);
+	    	alert.setContentText("No items added to transaction!");
 
+	    	alert.showAndWait();
+			
+		} else {
+			
 		completePopup.setVisible(true);
+		}
+
 
     }
 	
@@ -255,15 +245,24 @@ public class TransactionController implements Initializable {
     	
     	String selectedPerson = debtListCB.getSelectionModel().getSelectedItem();
 	    System.out.println("ComboBox Action (selected: " + selectedPerson.toString() + ")");
+	    
+	    if(selectedPerson.equalsIgnoreCase("New Customer in Debt")) {
+	    	
+	    } else {
+	    	
+	    	
+	    }
+	    
 
     }
 
     @FXML
-    void handleOK(ActionEvent event) {
+    void handleOK(ActionEvent event) throws SQLException {
     	
     	completePopup.setVisible(false);
     	
-    	float change = Float.parseFloat((paidTF.getText())) - Float.parseFloat(totalAmount.getText());
+    	
+    	float change = Float.parseFloat((paidTF.getText())) - Float.parseFloat(totalAmount.getText());    	
     	
     	Alert alert = new Alert(AlertType.INFORMATION);
     	alert.setTitle("Customer's Change");
@@ -271,74 +270,74 @@ public class TransactionController implements Initializable {
     	alert.setContentText("Change: ₱ "+ f.format(change));
 
     	alert.showAndWait();
-    	
+    	ArrayList<TransactionItem> tranItems = convertSalesTable(salesTable);
+    	model.newTransaction(tranItems, convertPaid());
     	paidTF.clear();
     	totalAmount.clear();
     	salesTable.getItems().clear();
-
+    }
+    
+    /*Pangit code ko sorry*/
+    private ArrayList<TransactionItem> convertSalesTable(TableView<SaleEntry> salesTable) throws SQLException{
+    	ArrayList<TransactionItem> tranItems = new ArrayList<>();
+    	for(int i = 0; i < salesTable.getItems().size(); i++){
+    		int transID = -1;
+    		int itemID = model.findItemByDesc(salesTable.getItems().get(i).getItemName()).getId();
+    		float unitPrice = salesTable.getItems().get(i).getUcost();
+    		int quantity = salesTable.getItems().get(i).getQty();
+    		TransactionItem tItem = new TransactionItem(transID, itemID, unitPrice, quantity, "bought");
+    		tranItems.add(tItem);
+    	}
+    	return tranItems;
+    }
+    
+    private int convertPaid(){
+    	if(Integer.parseInt(paidTF.getText()) > 0)
+    		return 1;
+    	return 0;
     }
     
     @FXML
     void handleCancel(ActionEvent event) {
-    	
-
     	completePopup.setVisible(false);
     	salesTable.getItems().clear();
 
     }
     
     @FXML
-    void handleCart(ActionEvent event) {
-    	
-//    	item = se.getItemName();
-//    	qty = 1;
-//    	ucost = se.getUcost();
-//    	totalAmt += Float.parseFloat(ucost) * qty;
-//    	f.format(totalAmt);
-//    	totalAmount.setText(f.format(totalAmt));
-//    	
-//    	generateSaleEntry(item, Float.parseFloat(ucost) * qty, qty, ucost);
-//    	
-//    	item = fe.getItemName();
-//    	qty = 1;
-//    	ucost = fe.getUcost();
-//    	totalAmt += Float.parseFloat(ucost) * qty;
-//    	f.format(totalAmt);
-//    	totalAmount.setText(f.format(totalAmt));
-//    	
-//    	generateSaleEntry(item, Float.parseFloat(ucost) * qty, qty, ucost);
-    	
+    void handleCart(ActionEvent event) throws SQLException {
     	newEntry = new SaleEntry();
-    	newEntry.itemName.set(itemText.getText());
-    	newEntry.qty.set(1);
+    	System.out.println("Add to cart clicked!");
+    	Item item = new Item();
+    	//ayaw gumana pag model nag call
+    	item = model.findItemByDesc(itemText.getText());
     	
-    	generateSaleEntry(newEntry);
+    	newEntry.itemName.set(item.getDesc());
+    	newEntry.qty.set(1);
+    	newEntry.setUcost(new SimpleFloatProperty(item.getUnitPrice()));
+    	
     	qty = newEntry.getQty();
     	ucost = newEntry.getUcost();
+    	generateSaleEntry(newEntry);
     	
     	setTotal();
-    	
     	itemText.clear();
     }
-
 	
 	private void generateSaleEntry(SaleEntry entry) {
-		
-//		SaleEntry entry = new SaleEntry();
-//	    entry.itemName.set(item);
-//	    entry.qty.set(qty);
-//	    entry.ucost.set(ucost);
-	    data.add(entry);
-	    int i = data.indexOf(entry);
-	    entry.price.set((setPrice(qty, ucost, i)));
-	    
+		data.add(entry);
+		int i = data.indexOf(entry);
+		entry.price.set((setPrice(qty, ucost, i)));
+	  
 	}
 	
 	private float setPrice(int newqty, float cost, int pos) {
 		
 		float price = 0;
+		System.out.println("Cost = " +cost +" New qty = " +newqty);
 		
 		price = cost * newqty ;
+		System.out.println("Price = "+price );
     	data.get(pos).setPrice(new SimpleFloatProperty(price));
     	return price;
 	
